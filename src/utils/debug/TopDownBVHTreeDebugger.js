c057acb3e2109a057b40c0b8b9b572d468bcc25d
Rui Gil <rui_web@hotmail.com>
2013-08-22 17:24:00 +0100

    Added TopDownBVHTreeDebugger to render debug information of the TopDownBVHTree map. Updated list of files to compile.

---
+++ src/utils/debug/TopDownBVHTreeDebugger.js
@@ -0,0 +1,21 @@
+Grape2D.TopDownBVHTreeDebugger = {
+	renderNode: function(node, renderer, camera) {
+		node.getBoundingVolume().render(renderer, camera);
+		renderer.renderText(node.getDepth(), camera.wcsToViewport(renderer, node.getBoundingVolume().getPosition()));
+		if (node.getLeft()) {
+			renderer.setStrokeColor("rgba(0,255,0,0.6)");
+			Grape2D.TopDownBVHTreeDebugger.renderNode(node.getLeft(), renderer, camera);
+			renderer.setStrokeColor("rgba(0,0,0,1)");
+		}
+		if (node.getRight()) {
+			renderer.setStrokeColor("rgba(0,0,255,0.6)");
+			Grape2D.TopDownBVHTreeDebugger.renderNode(node.getRight(), renderer, camera);
+			renderer.setStrokeColor("rgba(0,0,0,1)");
+		}
+	},
+	render: function(tree, renderer, camera) {
+		if (tree.getRootNode()) {
+			Grape2D.TopDownBVHTreeDebugger.renderNode(tree.getRootNode(), renderer, camera);
+		}
+	}
+};
