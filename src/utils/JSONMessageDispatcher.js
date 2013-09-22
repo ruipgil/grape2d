46be8d88368d4198a6c7a95e1a935a24078a2c45
unknown <rui_web@hotmail.com>
2013-09-22 14:36:22 +0100

    Added JSONMessageDispatcher.

---
+++ src/utils/JSONMessageDispatcher.js
@@ -0,0 +1,33 @@
+/**
+ * Dispatches JSON messages, according with a property.
+ *
+ * @param {!string} property Property to differentiate when
+ *   selecting the dispatcher.
+ * @extends {Grape2D.utils.MessageDispatcher}
+ * @constructor
+ */
+Grape2D.utils.JSONMessageDispatcher = function(property){
+	Grape2D.utils.MessageDispatcher.call(this);
+	this.property = property;
+};
+
+Grape2D.utils.JSONMessageDispatcher.prototype = Object.create(Grape2D.utils.MessageDispatcher.prototype);
+/**
+ * @override
+ */
+Grape2D.utils.JSONMessageDispatcher.prototype.dispatch = function(message, param){
+	var parsed, currentm, testP;
+	try{
+		parsed = JSON.parse(message);
+		testP = parsed[this.property];
+	}catch(e){
+		console.log(e);
+		return;
+	}
+	for (var i = 0; i < this.stack.length; i++) {
+		current = this.stack[i];
+		if (current.regex.test(testP)) {
+			this.stack[i].callback(parsed, param);
+		}
+	}
+};
