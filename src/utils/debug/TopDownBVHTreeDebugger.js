f2019d73c489a3508cfbfce2fced793b00b9a985
Rui Gil <rui_web@hotmail.com>
2013-09-25 15:05:37 +0100

    Fixed namespace change.

--- src/utils/debug/TopDownBVHTreeDebugger.js
+++ src/utils/debug/TopDownBVHTreeDebugger.js
@@ -4,18 +4,18 @@ Grape2D.utils.TopDownBVHTreeDebugger = {
 		renderer.renderText(node.getDepth(), camera.wcsToViewport(renderer, node.getBoundingVolume().getPosition()));
 		if (node.getLeft()) {
 			renderer.setStrokeColor("rgba(0,255,0,0.6)");
-			Grape2D.TopDownBVHTreeDebugger.renderNode(node.getLeft(), renderer, camera);
+			Grape2D.utils.TopDownBVHTreeDebugger.renderNode(node.getLeft(), renderer, camera);
 			renderer.setStrokeColor("rgba(0,0,0,1)");
 		}
 		if (node.getRight()) {
 			renderer.setStrokeColor("rgba(0,0,255,0.6)");
-			Grape2D.TopDownBVHTreeDebugger.renderNode(node.getRight(), renderer, camera);
+			Grape2D.utils.TopDownBVHTreeDebugger.renderNode(node.getRight(), renderer, camera);
 			renderer.setStrokeColor("rgba(0,0,0,1)");
 		}
 	},
 	render: function(tree, renderer, camera) {
 		if (tree.getRootNode()) {
-			Grape2D.TopDownBVHTreeDebugger.renderNode(tree.getRootNode(), renderer, camera);
+			Grape2D.utils.TopDownBVHTreeDebugger.renderNode(tree.getRootNode(), renderer, camera);
 		}
 	}
 };
