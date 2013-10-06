44383b35e9d4c0aa1e6cef31c0bbb0e298d41d69
unknown <rui_web@hotmail.com>
2013-10-07 00:12:22 +0100

    Fixed inability to instantiate with a non-generated id.

--- src/objects/NetworkObject2D.js
+++ src/objects/NetworkObject2D.js
@@ -8,7 +8,7 @@
  */
 Grape2D.NetworkObject2D = function(options) {
 	Grape2D.Object2D.call(this, options);
-	this.id = Grape2D.NetworkObject2DIdGeneratorSingleton.request();
+	this.id = options.id || Grape2D.NetworkObject2DIdGeneratorSingleton.request();
 };
 
 Grape2D.NetworkObject2D.prototype = Object.create(Grape2D.Object2D.prototype);
